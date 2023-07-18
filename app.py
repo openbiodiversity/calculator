53423c08740ef670f219d1ea21ecc0920b141de6
Micah Buuck <mbuuck@gmail.com>
2023-07-18 15:58:16 -0700

    Added an autozoom feature

--- app.py
+++ app.py
@@ -242,6 +242,49 @@ def create_dataframe(years, project_name):
         dfs.append(df)
     return pd.concat(dfs)
 
+# h/t: https://community.plotly.com/t/dynamic-zoom-for-mapbox/32658/12
+def get_plotting_zoom_level_and_center_coordinates_from_lonlat_tuples(longitudes=None, latitudes=None):
+    """Function documentation:\n
+    Basic framework adopted from Krichardson under the following thread:
+    https://community.plotly.com/t/dynamic-zoom-for-mapbox/32658/7
+
+    # NOTE:
+    # THIS IS A TEMPORARY SOLUTION UNTIL THE DASH TEAM IMPLEMENTS DYNAMIC ZOOM
+    # in their plotly-functions associated with mapbox, such as go.Densitymapbox() etc.
+
+    Returns the appropriate zoom-level for these plotly-mapbox-graphics along with
+    the center coordinate tuple of all provided coordinate tuples.
+    """
+
+    # Check whether both latitudes and longitudes have been passed,
+    # or if the list lenghts don't match
+    if ((latitudes is None or longitudes is None)
+            or (len(latitudes) != len(longitudes))):
+        # Otherwise, return the default values of 0 zoom and the coordinate origin as center point
+        return 0, (0, 0)
+
+    # Get the boundary-box 
+    b_box = {} 
+    b_box['height'] = latitudes.max()-latitudes.min()
+    b_box['width'] = longitudes.max()-longitudes.min()
+    b_box['center']= (np.mean(longitudes), np.mean(latitudes))
+
+    # get the area of the bounding box in order to calculate a zoom-level
+    area = b_box['height'] * b_box['width']
+
+    # * 1D-linear interpolation with numpy:
+    # - Pass the area as the only x-value and not as a list, in order to return a scalar as well
+    # - The x-points "xp" should be in parts in comparable order of magnitude of the given area
+    # - The zpom-levels are adapted to the areas, i.e. start with the smallest area possible of 0
+    # which leads to the highest possible zoom value 20, and so forth decreasing with increasing areas
+    # as these variables are antiproportional
+    zoom = np.interp(x=area,
+                     xp=[0, 5**-10, 4**-10, 3**-10, 2**-10, 1**-10, 1**-5],
+                     fp=[20, 15,    14,     13,     12,     7,      5])
+
+    # Finally, return the zoom level and the associated boundary-box center coordinates
+    return zoom, b_box['center']
+
 def show_project_map(project_name):
     prepared_statement = \
         con.execute("SELECT geometry FROM project WHERE name = ? LIMIT 1",
@@ -249,18 +292,19 @@ def show_project_map(project_name):
     features = \
         json.loads(prepared_statement[0][0].replace("\'", "\""))['features']
     geometry = features[0]['geometry']
-    x_centroid = np.mean(np.array(geometry["coordinates"])[0, :, 0])
-    y_centroid = np.mean(np.array(geometry["coordinates"])[0, :, 1])
+    longitudes = np.array(geometry["coordinates"])[0, :, 0]
+    latitudes = np.array(geometry["coordinates"])[0, :, 1]
+    zoom, bbox_center = get_plotting_zoom_level_and_center_coordinates_from_lonlat_tuples(longitudes, latitudes)
     fig = go.Figure(go.Scattermapbox(
         mode = "markers",
-        lon = [x_centroid], lat = [y_centroid],
+        lon = [bbox_center[0]], lat = [bbox_center[1]],
         marker = {'size': 20, 'color': ["cyan"]}))
 
     fig.update_layout(
         mapbox = {
             'style': "stamen-terrain",
-            'center': { 'lon': x_centroid, 'lat': y_centroid},
-            'zoom': 12, 'layers': [{
+            'center': { 'lon': bbox_center[0], 'lat': bbox_center[1]},
+            'zoom': zoom, 'layers': [{
                 'source': {
                     'type': "FeatureCollection",
                     'features': [{
